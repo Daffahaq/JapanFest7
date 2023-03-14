a17f939029b5e86f34b1164a25b91f71436f8a74
daffahaq <daffaolover@student.uns.ac.id>
2023-03-14 11:55:40 +0700

    menambahkan tugas exercise 1

---
+++ tugass.py
@@ -0,0 +1,12 @@
+print("""
+Nama : muhammad natan ydh
+NIM  : I03220086
+
+Saya tinggal di Tulungaung. Tempat tinggal saya terletak di pelosok
+desa, tepat di desa kepuh,kec boyolangu. 
+Di depan rumah saya terdapat beberapa penjual sembako kecil yang menjual barang barang sembako biasanya telur,beras,dan lain lain. di depan rumah saya juga terdapat 2 buah 
+pohon rambutan dan mangga. olah karena itu, rumah saya memiliki suasane yang ramai.
+
+Harapan saya ketika lulus dari teknik industri yaitu memiliki pekerjaan tetap yang sesuai
+dengan ilmu-ilmu yang sudah saya pelajari di teknik industri. 
+""")
